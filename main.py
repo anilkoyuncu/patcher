cc0f89a8fd9ba9202103b86c6e744f9679d217f8
ANIL KOYUNCU <anil.koyuncu@mp0360.uni.lux>
2020-05-25 13:26:25 +0200

    changes

--- main.py
+++ main.py
@@ -8,17 +8,13 @@ if __name__ == '__main__':
 
 
     try:
-        args = getRun()
+		args = getRun()
+		sys.path.append(args.root)
+		files = args.job
+		print(files)
+		ROOT_DIR = = args.root
+		cmd = 'spatch --sp-file ' + join(DATASET,'cocci',spfile) + ' ' + srcPath + ' --patch -o' + join(pathManyBug,manybug,'patches',patchName) + ' > ' + join(pathManyBug, manybug, 'patches', patchName + spfile + '.txt')
 		
-
-	    os.environ["ROOT_DIR"] = args.root
-	    sys.path.append(args.root)
-	    files = args.job
-	    print(files)
-	    ROOT_DIR = os.environ["ROOT_DIR"]
-	    cmd = 'spatch --sp-file ' + join(DATASET,'cocci',spfile) + ' ' + srcPath + ' --patch -o' + join(pathManyBug,manybug,'patches',patchName) + ' > ' + join(pathManyBug, manybug, 'patches', patchName + spfile + '.txt')
-
-
 	except Exception as e:
         logging.error(e)
                      
