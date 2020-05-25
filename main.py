ea242fd93e96956bf3a5c0df85ad71737bae09be
ANIL KOYUNCU <anil.koyuncu@mp0360.uni.lux>
2020-05-25 17:47:39 +0200

    changes

--- main.py
+++ main.py
@@ -16,7 +16,15 @@ if __name__ == '__main__':
 		print(files)
 		logging.info(files)
 		ROOT_DIR = args.root
-		# cmd = 'spatch --sp-file ' + join(DATASET,'cocci',spfile) + ' ' + srcPath + ' --patch -o' + join(pathManyBug,manybug,'patches',patchName) + ' > ' + join(pathManyBug, manybug, 'patches', patchName + spfile + '.txt')
+		spfile = 'for_14_10.cocci'
+
+		os.mkdir(join(ROOT_DIR,'patches'))
+		patchName = 'inflate.c'
+		srcPath = 'inflate.c'
+		cmd = 'spatch --sp-file ' + join(ROOT_DIR,'cocci',spfile) + ' ' + srcPath + ' --patch -o' + join(ROOT_DIR,'patches',patchName) + ' > ' + join(ROOT_DIR, 'patches', patchName + spfile + '.txt')
+		output, e = shellGitCheckout(cmd)
+		print(output)
+		print(e)
 
 	except Exception as e:
 		logging.error(e)
