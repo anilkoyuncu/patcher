c6b68132116153bf4493c8d4f38aba7ea99a1ddb
ANIL KOYUNCU <anil.koyuncu@mp0360.uni.lux>
2020-05-25 13:30:54 +0200

    changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from common.commons import *
 if __name__ == '__main__':
 
 
-    try:
+	try:
 		args = getRun()
 		sys.path.append(args.root)
 		files = args.job
@@ -16,5 +16,4 @@ if __name__ == '__main__':
 		cmd = 'spatch --sp-file ' + join(DATASET,'cocci',spfile) + ' ' + srcPath + ' --patch -o' + join(pathManyBug,manybug,'patches',patchName) + ' > ' + join(pathManyBug, manybug, 'patches', patchName + spfile + '.txt')
 
 	except Exception as e:
-        logging.error(e)
-                     
+		logging.error(e)
