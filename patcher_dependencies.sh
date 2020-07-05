96d130a5fb32de46d13ccb7ba41fcd9116377cb5
anil <anil.koyuncu@uni.lu>
2020-07-05 23:42:12 +0200

    test

--- patcher_dependencies.sh
+++ patcher_dependencies.sh
@@ -19,26 +19,33 @@ export USE_CCACHE=1
 conda env create -f environment.yml
 conda activate fixminerEnv
 
-ccache --version
-ccache --zero-stats
-export USE_CCACHE=1
-export OCAML_VERSION=4.05 
+git checkout -b test123456
+touch test
+git add .
+git commit -m 'test'
+git config --add git-pull-request.hosttype github
+git pull-request
 
-# test -e .travis-opam.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
-test -e .travis-ocaml.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-ocaml.sh
-bash -ex .travis-ocaml.sh
-eval $(opam config env)
-# bash -ex .travis-opam.sh
-# eval $(opam env)
-opam install coccinelle -y
-# - git remote set-url origin https://anilkoyuncu:${GITHUB_TOKEN}@github.com/anilkoyuncu/patcher.git
+# ccache --version
+# ccache --zero-stats
+# export USE_CCACHE=1
+# export OCAML_VERSION=4.05 
 
-# # CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
-# # echo $CHANGED_FILES
-# # echo ${GH_TOKEN}
-# # echo $GH_TOKEN
-# # # spatch --version
-# # bash patcher.sh $CHANGED_FILES
-echo $RANGE
-echo $TARGETREPO
-bash patcher.sh $RANGE $TARGETREPO
+# # test -e .travis-opam.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
+# test -e .travis-ocaml.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-ocaml.sh
+# bash -ex .travis-ocaml.sh
+# eval $(opam config env)
+# # bash -ex .travis-opam.sh
+# # eval $(opam env)
+# opam install coccinelle -y
+# # - git remote set-url origin https://anilkoyuncu:${GITHUB_TOKEN}@github.com/anilkoyuncu/patcher.git
+
+# # # CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
+# # # echo $CHANGED_FILES
+# # # echo ${GH_TOKEN}
+# # # echo $GH_TOKEN
+# # # # spatch --version
+# # # bash patcher.sh $CHANGED_FILES
+# echo $RANGE
+# echo $TARGETREPO
+# bash patcher.sh $RANGE $TARGETREPO
