95a1ae0a2a27e631a98243be464b54c5a81db0e6
anil <anil.koyuncu@uni.lu>
2020-07-06 12:38:11 +0200

    test pull-request

--- patcher_dependencies.sh
+++ patcher_dependencies.sh
@@ -22,18 +22,23 @@ export USE_CCACHE=1
 conda env create -f environment.yml
 conda activate fixminerEnv
 
-ccache --version
-ccache --zero-stats
-export USE_CCACHE=1
-export OCAML_VERSION=4.05 
+git checkout -b mytest
+touch test
+git add .
+git commit -m 'test'
+git pull-request
+# ccache --version
+# ccache --zero-stats
+# export USE_CCACHE=1
+# export OCAML_VERSION=4.05 
 
-# test -e .travis-opam.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
-test -e .travis-ocaml.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-ocaml.sh
-bash -ex .travis-ocaml.sh
-eval $(opam config env)
-# bash -ex .travis-opam.sh
-# eval $(opam env)
-opam install coccinelle -y
+# # test -e .travis-opam.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-opam.sh
+# test -e .travis-ocaml.sh || wget https://raw.githubusercontent.com/ocaml/ocaml-ci-scripts/master/.travis-ocaml.sh
+# bash -ex .travis-ocaml.sh
+# eval $(opam config env)
+# # bash -ex .travis-opam.sh
+# # eval $(opam env)
+# opam install coccinelle -y
 # - git remote set-url origin https://anilkoyuncu:${GITHUB_TOKEN}@github.com/anilkoyuncu/patcher.git
 
 # # CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
